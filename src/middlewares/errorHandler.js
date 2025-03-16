6762a242a50795990ca8e1494532dd4b3741d135
JessFreak <maliy.olexander@lll.kpi.ua>
2025-03-16 17:05:55 +0200

    resolved SonarQube issues

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,13 +1,13 @@
-import { getStatusText } from 'http-status-codes';
+import { ReasonPhrases, StatusCodes } from 'http-status-codes';
 
-export const errorHandler = (err, req, res, next) => {
-  const statusCode = err.status || 500;
+export const errorHandler = (err, req, res) => {
+  const statusCode = err.status || StatusCodes.INTERNAL_SERVER_ERROR;
 
   res.status(statusCode).json({
-    title: getStatusText(statusCode) || 'Internal Server Error',
+    title: ReasonPhrases[statusCode] || 'Internal Server Error',
     status: statusCode,
     message: err.message,
     path: req.originalUrl,
     timestamp: new Date().toISOString(),
   });
-}
+};
